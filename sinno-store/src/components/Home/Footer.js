624c9f75c319fd72c0e8ef71cef8bdd00ba09381
hathai25 <74005327+hathai25@users.noreply.github.com>
2020-11-10 17:08:11 +0700

    Update Footer.js

---
+++ sinno-store/src/components/Home/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const FooterPage = () => {
+  return (
+    <MDBFooter color="grey darken-2" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">SINNO</h5>
+            <p>
+              Câu lạc bộ là nơi học tập và tìm kiếm đam mê. 
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Products</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">IoT</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Apps</a>
+              </li>
+            </ul>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Contact</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Address: Phòng 407b, tòa nhà B1, Đại học Bách Khoa Hà Nộ</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Email: sinno@soict.hust.edu.vn</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="#"> SOICT Innovation Club - All Rights Reserved </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default FooterPage;
