0fbd9d00dcd98b4fa0243fed22003074fce3c321
minhhung123 <prohungnguyenminh@gmail.com>
2020-11-13 20:41:50 +0700

    Edit images

--- sinno-store/src/components/Home.js
+++ sinno-store/src/components/Home.js
@@ -7,6 +7,10 @@ import { About } from "./Share/About";
 import { Form } from "react-bootstrap";
 // image
 import web1 from './Share/images/Web1.jpg';
+import mobile from './Share/images/mobile.png';
+import IoT from './Share/images/IoT.jpg';
+import AI from './Share/images/AI.jpg';
+import others from './Share/images/others.jpg';
 
 
 export const Home = () => {
@@ -21,12 +25,12 @@ export const Home = () => {
                 <h2>Categories</h2>
                 <div className="row CateRow1">
                     <Cate col="col-lg-6" link="#" imgRowSrc={web1} imgRowAlt="Web" cateClass="product-title"/>
-                    <Cate col="col-lg-6" link="#" imgRowSrc="https://lh3.googleusercontent.com/proxy/CNh8Wo_03veXElh9c4rx5skv_vwydqCafMLtGesDLhklYJ0JIz-f1UQUZev4oOvGmKyx5vEm-6ZAYF49qBNWJSm5LrfzL-Id4BJW6c-SXcuqtWJe5fMq6-9gl8xSZTEIB5MyM9R_jZei7Dya5ItvG5C-LS5i" imgRowAlt="Mobile" cateClass="product-title"/>
+                    <Cate col="col-lg-6" link="#" imgRowSrc={mobile} imgRowAlt="Mobile" cateClass="product-title"/>
                 </div>
                 <div className="row CateRow2">
-                    <Cate col="col-lg-4" link="#" imgRowSrc="https://www.thegioimaychu.vn/blog/wp-content/uploads/2020/01/iot_internet_of_things_by_jackie_niam_gettyimages-996958260_2400x1600-100788446-large1-e1578563981761.jpg" imgRowAlt="IoT" cateClass="product-title"/>
-                    <Cate col="col-lg-4" link="#" imgRowSrc="https://cdnimg.vietnamplus.vn/uploaded/izhsa/2018_11_09/savvycomailab.jpg" imgRowAlt="AI" cateClass="product-title"/>
-                    <Cate col="col-lg-4" link="#" imgRowSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ81Ma6MABBqGHnTrHA_GfFbTup5siRYTEuUw&usqp=CAU" imgRowAlt="Others" cateClass="product-title"/>
+                    <Cate col="col-lg-4" link="#" imgRowSrc={IoT} imgRowAlt="IoT" cateClass="product-title"/>
+                    <Cate col="col-lg-4" link="#" imgRowSrc={AI} imgRowAlt="AI" cateClass="product-title"/>
+                    <Cate col="col-lg-4" link="#" imgRowSrc={others} imgRowAlt="Others" cateClass="product-title"/>
                 </div>
             </div>
 
