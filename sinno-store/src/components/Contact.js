00049244600659fab4a8419e7935d39b537a6434
hathai25 <74005327+hathai25@users.noreply.github.com>
2020-11-18 22:16:22 +0700

    Update Contact.js

--- sinno-store/src/components/Contact.js
+++ sinno-store/src/components/Contact.js
@@ -1,35 +1,29 @@
 import React from "react";
 import { NavBar } from "./Share/NavBar";
 import { Footer } from "./Share/Footer";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
+import "../contact.css"
 
-export const Contact = () => {
+const Contact = () => {
     return (
         <div className="Contact">
-            <NavBar />
-            <MDBContainer>
-                    <MDBRow>
-                    <MDBCol md="6">
-                        <form>
-                        <p className="h5 text-center mb-4">Write to us</p>
-                        <div className="grey-text">
-                            <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
-                            success="right" />
-                            <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
-                            success="right" />
-                            <MDBInput type="textarea" rows="2" label="Your message" icon="pencil-alt" />
-                        </div>
-                        <div className="text-center">
-                            <MDBBtn outline color="secondary">
-                            Send
-                            <MDBIcon far icon="paper-plane" className="ml-1" />
-                            </MDBBtn>
-                        </div>
-                        </form>
-                    </MDBCol>
-                    </MDBRow>
-            </MDBContainer>
-            <Footer/>
+        <NavBar />
+        <form className="form">
+            <h1>Give us feedbacks here!</h1>
+
+            <label>Name</label>
+            <input placeholder="name" />
+
+            <label>Email</label>
+            <input placeholder="Email" />
+
+            <label>Message</label>
+            <input placeholder="Message" />
+
+            <button type="submit">Submit</button>
+
+        </form>
+        <Footer/>
         </div>
     )
 }
+export default Contact
