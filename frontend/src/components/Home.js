c5af625861e9044b8ffca36b898de8c5dc7e9b69
Haipham2002 <seal.plh@gmail.com>
2020-11-21 12:00:09 +0700

    Intro

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -18,32 +18,33 @@ export const Home = () => {
         
         <div className="Home">
             <React.Fragment>
-            <NavBar />
-            <Intro />
-            
-            <div className="Cate">
-                <h2>Categories</h2>
-                <div className="row CateRow1">
-                    <Cate col="col-lg-6" link="#" imgRowSrc={web1} imgRowAlt="Web" cateClass="product-title"/>
-                    <Cate col="col-lg-6" link="#" imgRowSrc={mobile} imgRowAlt="Mobile" cateClass="product-title"/>
+                <NavBar />
+
+                <Intro />
+
+                <div className="About">
+                    <h1>About</h1>
+                    <About />
                 </div>
-                <div className="row CateRow2">
-                    <Cate col="col-lg-4" link="#" imgRowSrc={IoT} imgRowAlt="IoT" cateClass="product-title"/>
-                    <Cate col="col-lg-4" link="#" imgRowSrc={AI} imgRowAlt="AI" cateClass="product-title"/>
-                    <Cate col="col-lg-4" link="#" imgRowSrc={others} imgRowAlt="Others" cateClass="product-title"/>
+                
+                <div className="Cate">
+                    <h2>Categories</h2>
+                    <div className="row CateRow1">
+                        <Cate col="col-lg-6" link="#" imgRowSrc={web1} imgRowAlt="Web" cateClass="product-title"/>
+                        <Cate col="col-lg-6" link="#" imgRowSrc={mobile} imgRowAlt="Mobile" cateClass="product-title"/>
+                    </div>
+                    <div className="row CateRow2">
+                        <Cate col="col-lg-4" link="#" imgRowSrc={IoT} imgRowAlt="IoT" cateClass="product-title"/>
+                        <Cate col="col-lg-4" link="#" imgRowSrc={AI} imgRowAlt="AI" cateClass="product-title"/>
+                        <Cate col="col-lg-4" link="#" imgRowSrc={others} imgRowAlt="Others" cateClass="product-title"/>
+                    </div>
                 </div>
-            </div>
 
-            <hr />
+                <hr />
 
-            <div className="About">
-                <h1>About</h1>
-                <About />
-            </div>
-
-            <div className="Footer">
-                <Footer />
-            </div>
+                <div className="Footer">
+                    <Footer />
+                </div>
             
             </React.Fragment>
         </div>
--- frontend/src/components/Share/Intro.js
+++ frontend/src/components/Share/Intro.js
@@ -1,63 +1,37 @@
 import React from "react";
-import sinnoWorkshop from './images/sinnoWorkshop.jpg';
 import sinnoBrand from "./images/sinno-brand.jpg";
-import sinno1 from "./images/sinno1.jpg";
-import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from "mdbreact";
+import Button from 'react-bootstrap/Button';
+// import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from "mdbreact";
 
 const Intro = () => {
     return (
-        <MDBContainer>
-            <MDBCarousel
-                activeItem={1}
-                length={3}
-                showControls={true}
-                showIndicators={true}
-                className="z-depth-1"
-            >
-                <MDBCarouselInner>
-                    <MDBCarouselItem itemId="1">
-                        {/* <MDBView> */}
-                            <img
-                                className="d-block w-100 IntroImg"
-                                src={sinnoWorkshop}
-                                alt="First slide" />
-                            <MDBMask overlay="black-light" />
-                        {/* </MDBView> */}
-                        <MDBCarouselCaption>
-                            <h3 className="h3-responsive">Sinno products</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId="2">
-                        {/* <MDBView> */}
-                            <img
-                                className="d-block w-100 IntroImg"
-                                src={sinnoBrand}
-                                alt="Second slide" />
-                            <MDBMask overlay="black-strong" />
-                        {/* </MDBView> */}
-                        <MDBCarouselCaption>
-                            <h3 className="h3-responsive">SINNO SOICT</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId="3">
-                        {/* <MDBView> */}
-                            <img
-                                className="d-block w-100 IntroImg"
-                                src={sinno1}
-                                alt="Third slide" />
-                            <MDBMask overlay="black-slight" />
-                        {/* </MDBView> */}
-                        <MDBCarouselCaption>
-                            <h3 className="h3-responsive">Sinno workshop</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                </MDBCarouselInner>
-            </MDBCarousel>
-        </MDBContainer>
+        <div className="Intro">
+            <div className="">
+                <div className="container-fluid">
+                    <div className="row introRow">
+                    
+                        <div className="col-lg-6 introImgContainer">
+                            <img className="introImg" src={sinnoBrand} alt="sinno" />
+                        </div>
+
+                        <div className="col-lg-6 introContent">
+                            <div className="introTitle">
+                                <b>Sinno Store</b>
+                            </div>
+                            <div className="introDescribtion">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                            <div className="introButtonContainer">
+                                <Button className="introButton" variant="dark">
+                                    <div className="introButtonContent">Lorem</div>
+                                </Button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
+
 
 export { Intro };
