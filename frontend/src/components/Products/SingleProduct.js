7789fb7b46c6cb20f1dcc847cf81185a53a97544
Nguyễn Quang Tuấn <tuan-nguyenquang@users.noreply.github.com>
2020-11-24 17:44:48 +0700

    SingleProduct + Css + Data

--- frontend/src/components/Products/SingleProduct.js
+++ frontend/src/components/Products/SingleProduct.js
@@ -1,48 +1,110 @@
 import React from 'react';
 import { products } from '../../data';
 import { Link } from 'react-router-dom';
+import { NavBar } from '../Share/NavBar';
+import { Footer } from '../Share/Footer';
+import './SingleProduct Style/SingleProduct.css';
 import { Rating } from './Rating';
 
 export const SingleProduct = (props) => {
-    const product = products.find((x) => x._id === props.match.params.id) 
+    const product = products.find((x) => x._id === props.match.params.id)
     if (!product) {
         return <div>Product not found</div>
     }
     return (
-        <div>
-            <Link to="/apps">Back to results</Link>
-            <div className="row top">
-                <div className="col-5">
-                    <img className="large" src={product.image} alt={product.name}></img>
-                </div>
-                <div className="col-4">
-                    <ul>
-                        <li><h1>{product.name}</h1></li>
-                        <li><Rating rating={product.rating} numReviews={product.numReviews} /></li>
-                        <li>Price: ${product.price}</li>
-                        <li>Description: <p>{product.description}</p></li>
-                    </ul>
-                </div>
-                <div className="col-3">
-                    <div className="card card-body">
-                        <ul>
-                            <li>
-                                <div className="row">
-                                    <div>Price</div>
-                                    <div></div>
+        <div className="product-single-card">
+            <NavBar />
+            <section className="app-show-main product-main-page">
+                {/*<ShowMainHeader/>*/}
+                <div className="show-main-header">
+                    <div className="main-header-icon">
+                        <div className="header-icon-body">
+                            <img src={product.image} alt={product.name} />
+                        </div>
+                        {/*
+                       <div className="main-header-rec">
+                       <span>Internet of Thing</span>
+                       </div>
+                    */}
+                    </div>
+                    <div className="main-header-text">
+                        <div className="base-info-wrap">
+                            <h1>{product.name}</h1>
+                            <div className="header-text-author">
+                                <Link to="">
+                                    <span>Nhà phát triển: </span>
+                                    <span>{product.brand}</span>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="header-text-download">
+                            <div className="text-download-text">
+                                <div className="app-data-wrap">
+                                    <p className="description">
+                                        <span className="count-stats">{product.numsDownload} Downloads</span>
+                                    </p>
                                 </div>
-                            </li>
-                            <li>
-                                <div className="row"></div>
-                            </li>
-                            <li>
-                                <button className="primary block">Install</button>
-                            </li>
-                        </ul>
-                        
+                            </div>
+                            <div className="download-btn-wrap">
+                                <div className="product-button-download">
+                                    <button type="buttons" className="btn btn-primary">
+                                        <span>Download now</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                {/*<ShowMainBody />*/}
+                <div className="show-main-body collapse in first">
+                    <div className="main-body-common main-body-description">
+                        <div className="section-title">
+                            <h3>Description</h3>
+                        </div>
+                        <div className="text-wrapper">
+                            {product.description}
+                        </div>
+                    </div>
+                    <div className="main-body-common main-body-guide">
+                        <div className="section-title">
+                            <h3>
+                                How to Install
+                            </h3>
+                        </div>
+                        <div className="text-wrapper">
+                            {product.guide}
+                    </div>
+                    </div>
+                    <div className="main-body-common main-body-images">
+                        <div className="section-title">
+                            <h3>Images</h3>
+                        </div>
+                        <div className="body-images">
+                            {product.screenshot}
+                    </div>
+                    </div>
+                    <div className="main-body-common main-body-info">
+                        <div className="section-title">
+                            <h3>Detail info</h3>
+                        </div>
+                        <ul className="list-unstyled body-info-list">
+                            <li>
+                                <span className="info-item-title">File Size:</span>
+                                <span className="info-item-content">{product.filesize} MB</span>
+                            </li>
+                            <li>
+                                <span className="info-item-title">Update:</span>
+                                <span className="info-item-content">{product.updateTime}</span>
+                            </li>
+                            <li>
+                                <span className="info-item-title">Provider:</span>
+                                <span className="info-item-content">{product.brand}</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </div>
     )
 }
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,75 +1,16 @@
 export const products = [
     {
         _id: '1',
-        name: 'SinnoStore',
+        name: 'Product 1',
         category: 'Apps',
         image: 'https://didongviet.vn/statics/category/iphone/iphone-12/all-iphone-12.png',
-        price: 1000,
         brand: 'Sinno',
         rating: 5,
-        numReviews: 20,
-        description: 'high quality product',
-
+        numsDownload: 1234,
+        description: 'this is description',
+        guide: 'how to install product 1 -> See here',
+        filesize: 20,
+        screenshot: '',
+        updateTime: '',
     },
-    {
-        _id: '2',
-        name: 'Sinno IOT',
-        category: 'Iot',
-        image: 'https://niit.edu.vn/wp-content/uploads/2019/12/internet-of-things-la-gi-3.jpg',
-        price: 900,
-        brand: 'Sinno',
-        rating: 4.5,
-        numReviews: 10,
-        description: 'high quality product',
-
-    },
-    {
-        _id: '3',
-        name: 'Sinno IOT',
-        category: 'Iot',
-        image: 'https://niit.edu.vn/wp-content/uploads/2019/12/internet-of-things-la-gi-3.jpg',
-        price: 900,
-        brand: 'Sinno',
-        rating: 4.5,
-        numReviews: 10,
-        description: 'high quality product',
-
-    },
-    {
-        _id: '4',
-        name: 'Sinno IOT',
-        category: 'Iot',
-        image: 'https://niit.edu.vn/wp-content/uploads/2019/12/internet-of-things-la-gi-3.jpg',
-        price: 900,
-        brand: 'Sinno',
-        rating: 4.5,
-        numReviews: 10,
-        description: 'high quality product',
-
-    },
-    {
-        _id: '5',
-        name: 'Sinno IOT',
-        category: 'Iot',
-        image: 'https://niit.edu.vn/wp-content/uploads/2019/12/internet-of-things-la-gi-3.jpg',
-        price: 900,
-        brand: 'Sinno',
-        rating: 4.5,
-        numReviews: 10,
-        description: 'high quality product',
-
-    },
-    {
-        _id: '6',
-        name: 'Sinno IOT',
-        category: 'Iot',
-        image: 'https://niit.edu.vn/wp-content/uploads/2019/12/internet-of-things-la-gi-3.jpg',
-        price: 900,
-        brand: 'Sinno',
-        rating: 4.2,
-        numReviews: 10,
-        description: 'high quality product',
-
-    },
-    
 ]
